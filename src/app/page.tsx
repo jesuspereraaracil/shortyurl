2d34de706ccca91c99bf02829f3a8b3ff2ee4692
jperera <jpereraaracil@gmail.com>
2024-06-21 20:56:05 +0200

    chore(create database)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,20 @@ import { sql } from '@vercel/postgres';
 import React from 'react';
 
 export default async function Cart({ params }: { params: { user: string } }): Promise<React.JSX.Element> {
-  const { rows } = await sql`SELECT * from CARTS where user_id = ${params.user}`;
+  const { rows } = await sql`SELECT *
+                             from CARTS
+                             where user_id = ${params.user}`;
 
   return (
-    <div>
-      {rows.map((row) => (
-        <div key={row.id}>
-          {row.id} - {row.quantity}
-        </div>
-      ))}
-    </div>
+    <>
+      <div>TEST</div>
+      <div>
+        {rows.map((row) => (
+          <div key={row.id}>
+            {row.id} - {row.quantity}
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
