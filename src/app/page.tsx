fe5417ab274c6331cd2ae628d72240c6ee3fc520
jperera <jpereraaracil@gmail.com>
2024-06-21 20:50:48 +0200

    feat(initial configuration)

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import { sql } from '@vercel/postgres';
+import React from 'react';
+
+export default async function Cart({ params }: { params: { user: string } }): Promise<React.JSX.Element> {
+  const { rows } = await sql`SELECT * from CARTS where user_id = ${params.user}`;
+
+  return (
+    <div>
+      {rows.map((row) => (
+        <div key={row.id}>
+          {row.id} - {row.quantity}
+        </div>
+      ))}
+    </div>
+  );
+}
