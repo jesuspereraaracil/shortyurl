b1bf7831c2ba58db6f8544db37930fce7eab4e8f
jperera <jpereraaracil@gmail.com>
2024-06-22 11:44:48 +0200

    chore(add some logging)

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,12 +6,15 @@ export const Navbar = async () => {
   const session = await auth();
 
   if (!session) {
+    console.log('[Navbar] no session');
     return (
       <div className="container my-2 flex gap-2">
         <ServerAction action={login} copy={'LOGIN'} />
       </div>
     );
   }
+
+  console.log('[Navbar] session: ', session.user?.name);
 
   return (
     <div className="container my-2 flex items-center gap-2">
